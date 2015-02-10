ceceec06aa07e2460389e51097067060997035d4
Gabriel Agamennoni <gabriel.agamennoni@mavt.ethz.ch>
2015-02-10 16:22:48 +0100

    Added placeholder for bagfile processor

---
+++ tools/process_bagfile.cpp
@@ -0,0 +1,17 @@
+
+#include "ros/ros.h"
+
+# MAVLink header.
+#include "mavlink/v1.0/ASLUAV/mavlink.h"
+
+# ROS-generated message headers.
+#include "sensor_msgs/Imu.h"
+#include "sensor_msgs/MagneticField.h"
+#include "sensor_msgs/Temperature.h"
+#include "sensor_msgs/FluidPressure.h"
+#include "sensor_msgs/NavSatFix.h"
+
+int main(int num_arguments, char **arguments) {
+  
+  return 0;
+}
